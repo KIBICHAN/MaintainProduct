0ff1ea1aea72a30857e75f355111449fc0987a83
KIBICHAN <52041113+KIBICHAN@users.noreply.github.com>
2021-03-26 19:52:26 +0700

    sonarcloud

---
+++ MaintainProducts/Product.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaintainProducts
+{
+    //Khai bao lop Product
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public float UnitPrice { get; set; }
+        public int Quantity { get; set; }
+
+    }//end class
+    //Khai bao lop thuc hien cac thao tac: them, xoa, tim
+    public class ProductData
+    {
+        private List<Product> ProductsList = new List<Product>();
+        //Khai bao properties
+        public List<Product> GetProductsList
+        {
+            get
+            {
+                return ProductsList;
+            }
+        }
+
+        //Khai bao phuong thuc them Product
+        public void AddNew(Product p)
+        {
+            Product pro = Find(p.ProductID);
+            if(pro == null)
+            {
+                ProductsList.Add(p);
+            }
+            else
+            {
+                throw new Exception("Product is already exists.");
+            }
+        }
+
+        //Khai bao phuong thuc xoa Product
+        public void Remove(int ProductID)
+        {
+            Product p = Find(ProductID);
+            if(p != null)
+            {
+                ProductsList.Remove(p);
+            }
+            else
+            {
+                throw new Exception("Product is not already exists.");
+            }
+        }
+        //Khai bao phuong thuc tim products
+        public Product Find(int ProductID)
+        {
+            Product p = ProductsList.SingleOrDefault
+                (pro => pro.ProductID == ProductID);
+            return p;
+        }//end class
+    }//end namespace
+}
---
+++ MaintainProducts/frmMain.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MaintainProducts
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            //code
+            frmMain main = new frmMain();
+            main.ShowDialog();
+        }
+        ProductData data = new ProductData();
+
+        private void btnAddNew_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int ProID = int.Parse(txtProductID.Text);
+                string ProName = txtProductName.Text;
+                float Price = float.Parse(txtUnitPrice.Text);
+                int Quantity = int.Parse(txtQuantity.Text);
+                //Tao doi tuong Product
+                Product p = new Product { ProductID = ProID, ProductName = ProName, UnitPrice = Price, Quantity = Quantity };
+                data.AddNew(p);
+                //hien thi len GridView
+                dgvProducts.DataSource = null;
+                dgvProducts.DataSource = data.GetProductsList;
+            }catch(Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int ProID = int.Parse(txtProductID.Text);
+                data.Remove(ProID);
+                dgvProducts.DataSource = null;
+                dgvProducts.DataSource = data.GetProductsList;
+            }
+            catch(Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            int ProID = int.Parse(txtProductID.Text);
+            Product p = data.Find(ProID);
+            if (p == null)
+            {
+                MessageBox.Show("Product not found");
+            }
+            else
+            {
+                txtProductName.Text = p.ProductName;
+                txtQuantity.Text = p.Quantity.ToString();
+                txtUnitPrice.Text = p.UnitPrice.ToString();
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtProductID.Text = string.Empty;
+            txtProductName.Text = string.Empty;
+            txtQuantity.Text = string.Empty;
+            txtUnitPrice.Text = string.Empty;
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }//end class
+}//end namespace
